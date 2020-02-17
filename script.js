26f460fe77a83357b2f21862ae7337a012e0a882
Vinicius Rosa <bakavini99@gmail.com>
2020-02-17 15:12:47 -0300

    computed properties and watchers

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
 const data = {
-    title: 'Loops',
-    users: {
-        Joao: 'admin',
-        Thiago: 'editor',
-        Pafuncio: 'guest',
-    },
-    todos: [
-        {
-            id: '1',
-            title: 'Wash my possante!',
-            complete: 'false',
-        },
-        {
-            id: '2',
-            title: 'Buy some milk',
-            complete: 'true'
-        }
-    ]
-}
+    question: '',
+    answer: 'Eu não posso responder nada se você não me perguntar',
+};
 
 const vm = new Vue({
     el: '#app',
     data, // = data: data
+
+    methods: {
+        getAnswer(){
+            if (!this.question.includes('?')){
+                this.answer = 'Puets! Perguntas geralmente tem interrogação xD'
+                return
+            }
+
+            axios.get('https://yesno.wtf/api')
+                .then(res => {
+                    this.answer = res.data.answer;
+                })
+                .catch(err => {
+                    this.answer = 'Ops! Ocorreu algum erro :( ' + err
+                })
+        }
+    },
+
+    created(){
+        this.debouncedGetAnswer = _.debounce(this.getAnswer,500);
+    },
+
+    watch: {
+        question(){
+            this.answer = 'Digitando...';
+            this.debouncedGetAnswer();
+        }
+    }
 });
